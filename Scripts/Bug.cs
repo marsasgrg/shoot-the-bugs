2e178e2437e963edf0f9147dfcd49de93ea0bcb1
marsasgrg <92935256+marsasgrg@users.noreply.github.com>
2021-11-06 17:00:50 +0200

    Bug

--- Scripts/Bug.cs
+++ Scripts/Bug.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Bug : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public int speedX;
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(speedX,0 , 0);
     }
 }
