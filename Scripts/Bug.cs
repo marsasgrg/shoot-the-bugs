3750fd72fe01adf6e9a8af3b2133e38d90db3ae4
marsasgrg <92935256+marsasgrg@users.noreply.github.com>
2021-11-06 17:35:50 +0200

    Only 50 bugs available

--- Scripts/Bug.cs
+++ Scripts/Bug.cs
@@ -8,32 +8,43 @@ public class Bug : MonoBehaviour
     bool dup1;
     bool dup2;
     bool dup3;
+    public static int bugCount = 0;
     // Update is called once per frame
     void Update()
     {
         transform.Translate(speedX,0 , 0);
-        if (transform.position.x > -5){
-            if (!dup1){
-                Instantiate(this);
-                dup1 = true;
-            }
-            else if (transform.position.x > -1)
+        if (bugCount < 50) {
+            if (transform.position.x > -5)
             {
-                if (!dup2)
+                if (!dup1)
                 {
                     Instantiate(this);
-                    dup2 = true;
+                    dup1 = true;
+                    bugCount += 1;
+                    
+                    
                 }
-                else if (transform.position.x > 3)
+                else if (transform.position.x > -1)
                 {
-                    if (!dup3)
+                    if (!dup2)
                     {
                         Instantiate(this);
-                        dup3 = true;
+                        dup2 = true;
+                        bugCount += 1;
+                    }
+                    else if (transform.position.x > 3)
+                    {
+                        if (!dup3)
+                        {
+                            Instantiate(this);
+                            dup3 = true;
+                            bugCount += 1;
+                        }
                     }
                 }
             }
         }
+        
     }
     private void Awake()
     {
@@ -46,5 +57,6 @@ public class Bug : MonoBehaviour
         result *= 7;
         result -= 3.5;
         transform.position = new Vector3(-7, System.Convert.ToSingle(result), 0);
+        Debug.Log(bugCount.ToString());
     }
 }
