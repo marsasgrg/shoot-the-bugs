e3348e73563de71ca3df602e7fc015346665ae68
marsasgrg <92935256+marsasgrg@users.noreply.github.com>
2021-11-06 17:27:08 +0200

    Bug Randomisation 1

--- Scripts/Bug.cs
+++ Scripts/Bug.cs
@@ -4,10 +4,44 @@ using UnityEngine;
 
 public class Bug : MonoBehaviour
 {
-    public int speedX;
+    public float speedX;
+    bool dup1;
+    bool dup2;
+    bool dup3;
     // Update is called once per frame
     void Update()
     {
         transform.Translate(speedX,0 , 0);
+        if (transform.position.x > -5){
+            if (!dup1){
+                Instantiate(this);
+            }
+            else if (transform.position.x > -1)
+            {
+                if (!dup2)
+                {
+                    Instantiate(this);
+                }
+                else if (transform.position.x > 3)
+                {
+                    if (!dup3)
+                    {
+                        Instantiate(this);
+                    }
+                }
+            }
+        }
+    }
+    private void Awake()
+    {
+        dup1 = false;
+        dup2 = false;
+        dup3 = false;
+        double result;
+        System.Random rd = new System.Random();
+        result = rd.NextDouble();
+        result *= 7;
+        result -= 3.5;
+        transform.position = new Vector3(-7, System.Convert.ToSingle(result), 0);
     }
 }
