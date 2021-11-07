6b403bcd729de7bb0911b1470a2037ddd76abfb3
marsasgrg <92935256+marsasgrg@users.noreply.github.com>
2021-11-07 16:31:02 +0200

    Update gun

---
+++ Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public bool flying;
+    public float speed;
+    public void StartFlying()
+    {
+        this.gameObject.SetActive(true);
+        flying = true;
+    }
+    private void Update()
+    {
+        if (flying)
+        {
+            transform.Translate(-speed, 0, 0);
+        }
+    }
+}
+
+
--- Scripts/Gun.cs
+++ Scripts/Gun.cs
@@ -3,20 +3,22 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     public float speed;
+    public GameObject bullet;
     // Start is called before the first frame update
     void Bullet()
     {
-
+        GameObject newbull = Instantiate(bullet);
+        newbull.GetComponent<Bullet>().StartFlying();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float speedY = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float speedY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         transform.Translate(0, speedY, 0);
         if (Input.GetKeyDown(KeyCode.Space))
         {
-
+            Bullet();
         }
     }
 }
