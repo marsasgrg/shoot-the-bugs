e0d47176ecab6d240f3436a311a9130bbea57c62
marsasgrg <92935256+marsasgrg@users.noreply.github.com>
2021-11-07 16:10:40 +0200

    Gun 3

--- Scripts/Gun.cs
+++ Scripts/Gun.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
-    void Start()
+    void Bullet()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float speedY = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(0, speedY, 0);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
     }
 }
